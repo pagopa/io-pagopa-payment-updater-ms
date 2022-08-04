fd671f75b24c43acc413d8b81d1763e882b8e139
AleDore <alessio85.dore@gmail.com>
2022-08-04 18:34:08 +0200

    add log

--- src/main/java/it/gov/pagopa/paymentupdater/consumer/MessageKafkaConsumer.java
+++ src/main/java/it/gov/pagopa/paymentupdater/consumer/MessageKafkaConsumer.java
@@ -35,25 +35,27 @@ public class MessageKafkaConsumer {
 	private String payload = null;
 
 	@KafkaListener(topics = "${kafka.message}", groupId = "consumer-message", containerFactory = "kafkaListenerContainerFactory", autoStartup = "${message.auto.start}")
-	public void messageKafkaListener(Payment reminder)
+	public void messageKafkaListener(Payment paymentMessage)
 			throws JsonProcessingException, InterruptedException, ExecutionException {
-		log.info("Processing messageId=" + reminder.getId() + " time=" + new Date().toString());
-		if (Objects.nonNull(reminder) && Objects.nonNull(reminder.getContent_type())
-				&& reminder.getContent_type().equals(MessageContentType.PAYMENT)) {
-			log.debug("Received message with id: {} ", reminder.getId());
-			checkNullInMessage(reminder);
-			payload = reminder.toString();
+		log.info("Processing messageId=" + paymentMessage.getId() + " time=" + new Date().toString() + "paymentMessage="
+				+ paymentMessage.toString());
+		if (Objects.nonNull(paymentMessage) && Objects.nonNull(paymentMessage.getContent_type())
+				&& paymentMessage.getContent_type().equals(MessageContentType.PAYMENT)) {
+			log.debug("Received message with id: {} ", paymentMessage.getId());
+			checkNullInMessage(paymentMessage);
+			payload = paymentMessage.toString();
 			var pp = paymentService.getPaymentByNoticeNumberAndFiscalCode(
-					reminder.getContent_paymentData_noticeNumber(), reminder.getContent_paymentData_payeeFiscalCode());
+					paymentMessage.getContent_paymentData_noticeNumber(),
+					paymentMessage.getContent_paymentData_payeeFiscalCode());
 			if (pp.isEmpty()) {
-				String rptId = reminder.getContent_paymentData_payeeFiscalCode()
-						.concat(reminder.getContent_paymentData_noticeNumber());
-				reminder.setRptId(rptId);
+				String rptId = paymentMessage.getContent_paymentData_payeeFiscalCode()
+						.concat(paymentMessage.getContent_paymentData_noticeNumber());
+				paymentMessage.setRptId(rptId);
 				Map<String, Boolean> map = paymentServiceImpl.checkPayment(rptId);
 				if (map.containsKey("isPaid")) {
-					reminder.setPaidFlag(map.get("isPaid"));
+					paymentMessage.setPaidFlag(map.get("isPaid"));
 				}
-				paymentService.save(reminder);
+				paymentService.save(paymentMessage);
 			}
 		}
 
