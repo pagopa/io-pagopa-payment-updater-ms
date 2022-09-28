118971a824a8994196a85f8854a551982a3beacc
gparente3 <giovanni.parente@dxc.com>
2022-09-28 11:02:20 +0200

    fixed LongToLocalDateTimeMongoDbConverter

--- src/main/java/it/gov/pagopa/paymentupdater/config/LongToLocalDateTimeMongoDbConverter.java
+++ src/main/java/it/gov/pagopa/paymentupdater/config/LongToLocalDateTimeMongoDbConverter.java
@@ -8,15 +8,12 @@ import java.util.TimeZone;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.ReadingConverter;
 
-import it.gov.pagopa.paymentupdater.util.PaymentUtil;
-
 @ReadingConverter
 public class LongToLocalDateTimeMongoDbConverter implements Converter<Long, LocalDateTime> {
 
   @Override
   public LocalDateTime convert(Long source) {
-	  //getLocalDateTime() is mandatory to avoid server time zone problems
-	 return Optional.ofNullable(source).map(date -> PaymentUtil.getLocalDateTime(LocalDateTime.ofInstant(Instant.ofEpochSecond(source),
-              TimeZone.getDefault().toZoneId()).toLocalDate())).orElse(null);
+	 return Optional.ofNullable(source).map(date -> LocalDateTime.ofInstant(Instant.ofEpochSecond(source),
+              TimeZone.getDefault().toZoneId())).orElse(null);
   }
 }
